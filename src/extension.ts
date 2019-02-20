775f3398ad3dcd426b4f5dcbd49da97baa5bd890
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2019-02-20 14:10:06 +0100

    Render of webpage with current image  inside a webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { imageFetcher } from './pageFetcher';
+import { imageExtractor } from './imageExtractor';
+import { pageBuilder } from './pageBuilder';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "buongiorno-signora" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	console.log('Congratulations, your extension "buongiorno-signora" is now active!');
+	let disposable = vscode.commands.registerCommand('extension.bonjourMadame', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'bonjourMadame',
+			'Buongiorno signora',
+			vscode.ViewColumn.One,
+			{
+				retainContextWhenHidden: true
+			}
+		);
+		imageFetcher('http://bonjourmadame.fr')
+			.then(page => {
+			const todayImage = imageExtractor(page);
+			panel.webview.html = pageBuilder(todayImage);
+		})
 	});
 
 	context.subscriptions.push(disposable);
